05dfc9c2f1880e80819ec005f246dd4e18cd0d4e
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2019-08-10 15:22:45 +0500

    Fixed spectator mode, updated forge to 28.0.45, version bump

--- src/main/java/com/denismasterherobrine/angelring/ring/FlyEvent.java
+++ src/main/java/com/denismasterherobrine/angelring/ring/FlyEvent.java
@@ -3,9 +3,9 @@ package com.denismasterherobrine.angelring.ring;
 import com.denismasterherobrine.angelring.register.ItemRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class FlyEvent {
     @SubscribeEvent
@@ -14,9 +14,9 @@ public class FlyEvent {
         if(event.player.inventory.hasItemStack(new ItemStack(ItemRegistry.ItemRing))){
             event.player.abilities.allowFlying = true;
         }else{
-            if(event.player.abilities.isFlying == true && !event.player.inventory.hasItemStack(new ItemStack(ItemRegistry.ItemRing))){
-                event.player.abilities.isFlying = event.player.abilities.isCreativeMode ? true : false;
-                event.player.abilities.allowFlying = event.player.abilities.isCreativeMode ? true : false;
+            if(event.player.abilities.isFlying == true && !event.player.inventory.hasItemStack(new ItemStack(ItemRegistry.ItemRing)) && !event.player.isCreative() && !event.player.isSpectator()){
+                event.player.abilities.isFlying = false;
+                event.player.abilities.allowFlying = false;
             }
         }
     }
