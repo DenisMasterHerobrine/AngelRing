c1f5a268589a6419c41d6e8eb54e914837d89fc4
MelanX <meissi97@me.com>
2019-11-24 17:24:38 +0100

    added curios support

--- src/main/java/com/denismasterherobrine/angelring/ring/FlyEvent.java
+++ src/main/java/com/denismasterherobrine/angelring/ring/FlyEvent.java
@@ -1,22 +1,26 @@
 package com.denismasterherobrine.angelring.ring;
 
 import com.denismasterherobrine.angelring.register.ItemRegistry;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.CuriosAPI;
+
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class FlyEvent {
     @SubscribeEvent
     public static void onTickPlayerEvent(TickEvent.PlayerTickEvent event){
-        PlayerEntity player = (PlayerEntity) event.player;
-        if(event.player.inventory.hasItemStack(new ItemStack(ItemRegistry.ItemRing))){
-            event.player.abilities.allowFlying = true;
+        PlayerEntity player = event.player;
+        ItemStack angelRing = new ItemStack(ItemRegistry.ItemRing);
+        if(player.inventory.hasItemStack(angelRing) || CuriosAPI.getCurioEquipped(angelRing.getItem(), (LivingEntity) player.getEntity()).isPresent()){
+            player.abilities.allowFlying = true;
         }else{
-            if(event.player.abilities.isFlying == true && !event.player.inventory.hasItemStack(new ItemStack(ItemRegistry.ItemRing)) && !event.player.isCreative() && !event.player.isSpectator()){
-                event.player.abilities.isFlying = false;
-                event.player.abilities.allowFlying = false;
+            if(player.abilities.isFlying && !player.inventory.hasItemStack(angelRing) && !player.isCreative() && !player.isSpectator()){
+                player.abilities.isFlying = false;
+                player.abilities.allowFlying = false;
             }
         }
     }
--- src/main/java/com/denismasterherobrine/angelring/ring/ItemRing.java
+++ src/main/java/com/denismasterherobrine/angelring/ring/ItemRing.java
@@ -2,14 +2,24 @@ package com.denismasterherobrine.angelring.ring;
 
 import com.denismasterherobrine.angelring.AngelRing;
 import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.registries.ObjectHolder;
+import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 @Mod.EventBusSubscriber(modid = AngelRing.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(AngelRing.MODID)
 public class ItemRing extends Item {
     public ItemRing() {
         super(new Item.Properties().maxStackSize(1));
+    }
+
+    @SubscribeEvent
+    public static void sendImc(InterModEnqueueEvent event) {
+        InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("ring"));
     }
 }
 
