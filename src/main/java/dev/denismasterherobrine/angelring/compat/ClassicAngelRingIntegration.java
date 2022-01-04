46f9b5d4e10087ee83dcbe5739203bd8acc6a9bd
DenisMasterHerobrine <masterherobrine59@gmail.com>
2022-01-05 01:55:57 +0500

    Implemented Classic Mode for Angel Ring.

--- src/main/java/dev/denismasterherobrine/angelring/compat/ClassicAngelRingIntegration.java
+++ src/main/java/dev/denismasterherobrine/angelring/compat/ClassicAngelRingIntegration.java
@@ -87,14 +87,14 @@ public class ClassicAngelRingIntegration {
                     if (!player.abilities.mayfly && ExperienceUtils.getPlayerXP(player) > Configuration.XPCost.get()) {
                         startFlying(player);
                     }
-                    if (ExperienceUtils.getPlayerXP(player) <= Configuration.XPCost.get()){
+                    if (ExperienceUtils.getPlayerXP(player) <= Configuration.XPCost.get() && Configuration.XPCost.get() != 0){
                         stopFlying(player);
                         if (player instanceof ServerPlayerEntity){
                             // TODO: Localize it through i18n.
                             ((ServerPlayerEntity) player).sendMessage(new StringTextComponent("You need to have XP to fly. Get more XP to fly or take off Angel Ring."), ChatType.GAME_INFO, player.getUUID());
                         }
                     }
-                    if (player.abilities.mayfly && player.abilities.flying && ExperienceUtils.getPlayerXP(player) > Configuration.XPCost.get()) {
+                    if (player.abilities.mayfly && player.abilities.flying && ExperienceUtils.getPlayerXP(player) > Configuration.XPCost.get() && Configuration.XPCost.get() != 0) {
                         i++;
                         if (i >= Configuration.TicksPerDrain.get()){
                             ExperienceUtils.addPlayerXP(player, -Configuration.XPCost.get());
--- src/main/java/dev/denismasterherobrine/angelring/ring/ItemRing.java
+++ src/main/java/dev/denismasterherobrine/angelring/ring/ItemRing.java
@@ -2,6 +2,7 @@ package dev.denismasterherobrine.angelring.ring;
 
 import dev.denismasterherobrine.angelring.AngelRing;
 import dev.denismasterherobrine.angelring.compat.ClassicAngelRingIntegration;
+import dev.denismasterherobrine.angelring.config.Configuration;
 import dev.denismasterherobrine.angelring.utils.ExternalMods;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.ITooltipFlag;
@@ -51,10 +52,14 @@ public class ItemRing extends Item {
     public void appendHoverText(ItemStack itemStack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag tooltipFlag) {
         if (!Screen.hasShiftDown())
             tooltip.add(new StringTextComponent("Hold Shift for more information!").withStyle(TextFormatting.GRAY));
-        if (Screen.hasShiftDown()){
+        if (Screen.hasShiftDown() && Configuration.XPCost.get() != 0){
             tooltip.add(new StringTextComponent("Angel Ring drains player's XP over time while flying.").withStyle(TextFormatting.DARK_GREEN));
             tooltip.add(new StringTextComponent("If player's XP reaches less than 1 level,").withStyle(TextFormatting.GRAY));
             tooltip.add(new StringTextComponent("magic will disappear and flight will not be possible.").withStyle(TextFormatting.GRAY));
+        }
+        if (Screen.hasShiftDown() && Configuration.XPCost.get() == 0){
+            tooltip.add(new StringTextComponent("Angel Ring is switched into Classic Mode. Grants creative flight while in Curios slot.").withStyle(TextFormatting.RED));
+            tooltip.add(new StringTextComponent("Switch XP draining to more than 0 points in the config to switch to Rebalanced Mode.").withStyle(TextFormatting.RED));
         }
     }
 }
