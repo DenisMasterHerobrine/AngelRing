66ac2cdb209c79ebe1cd8b67330149da3c76f31d
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2024-11-09 15:23:51 +0500

    :skull:

--- src/main/java/dev/denismasterherobrine/angelring/compat/curios/abstraction/AbstractRingCurio.java
+++ src/main/java/dev/denismasterherobrine/angelring/compat/curios/abstraction/AbstractRingCurio.java
@@ -17,7 +17,7 @@ import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.SlotResult;
 import top.theillusivec4.curios.api.type.capability.ICurio;
-ing
+
 import java.util.Optional;
 
 public abstract class AbstractRingCurio implements ICurio {
